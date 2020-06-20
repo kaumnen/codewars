b4c99fbbdee47aedfc1c3f6f716742b9030b4a6e
Aleksej Komnenović <57869668+komnen0v1c@users.noreply.github.com>
2020-06-20 14:30:48 +0200

    Create [6 kyu] Detect Pangram.py

---
+++ [6 kyu] Detect Pangram.py
@@ -0,0 +1,9 @@
+def is_pangram(s):
+    arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    for i in s.lower():
+        try:
+            arr.remove(i)
+        except:
+            pass
+    return arr == []
